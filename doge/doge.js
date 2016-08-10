4d47af487eb98c6b4f3abb3f6451ccffd8d0a241
mathguy350 <mathguy350@gmail.com>
2016-08-10 11:44:18 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -12,12 +12,20 @@ class BootState {
 class LoadState {
   preload() {
     console.log("Loading...");
+    this.load.image("background","assets/background.png");
   }
   create() {
     console.log("Loaded");
   }
 }
 
+class PlayState {
+  create() {
+    console.log("Entering Play State");
+    this.background = this.add.tileSprite(0,0,320,568,"background");
+    this.background.autoScroll(0,700);
+  }
+}
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
 game.state.add("Load", LoadState);
