a1cea5248c7a9c6829bdbd898867ed17fa53a76c
mathguy350 <mathguy350@gmail.com>
2016-08-10 11:14:24 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -5,6 +5,15 @@ class BootState {
     this.scale.pageAlignVertically = true;
   }
 }
+
+class LoadState {
+  create() {
+    console.log("Loading");
+  }
+}
+
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
 game.state.start("Boot");
+game.state.add("Load", LoadState);
+game.state.start("Load");
