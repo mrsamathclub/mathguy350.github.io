a334fd6756000bde19491c5f7f3082c8cf9ee0b7
mathguy350 <mathguy350@gmail.com>
2016-08-10 11:17:50 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -7,13 +7,16 @@ class BootState {
 }
 
 class LoadState {
-  create() {
-    console.log("Loading");
+  preload() {
+    console.log("Loading...");
+  }
+  create {
+    console.log("Loaded")
   }
 }
 
 var game = new Phaser.Game(320,568);
 game.state.add("Boot",BootState);
-game.state.start("Boot");
 game.state.add("Load", LoadState);
-game.state.start("Load");
+game.state.start("Boot");
+
