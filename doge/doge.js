cf824485813d519090a3388ac162764a2256a1d3
mathguy350 <mathguy350@gmail.com>
2016-08-10 11:06:08 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -1,1 +1,10 @@
-var game = new Phaser.Game(320.568);
+class BootState {
+  preload() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically = true;
+  }
+}
+var game = new Phaser.Game(320,568);
+game.state.add("Boot",BootState);
+game.state.start("Boot");
