31914fb311f79bd27fe0702fafb7d8013b39f254
mathguy350 <mathguy350@gmail.com>
2016-08-11 11:35:58 -0400

    Update doge.js

--- doge/doge.js
+++ doge/doge.js
@@ -72,6 +72,14 @@ class PlayState {
     this.dodge.scale.set(1);
     this.dodge.animations.add("anim");
     this.dodge.animations.play("anim",C.d.fps,true);
+    
+    update() {
+    if (this.cursors.left.isDown) {
+      this.player.x -= C.p.speed;
+    }
+    if (this.cursors.right.isDown) {
+      this.player.x += C.p.speed;
+    }
   }
 }
 
